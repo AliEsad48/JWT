7ab8a36e25e04027d1de74ca334986bf74c5ad55
Salih Bayram <alicamci2020@gmail.com>
2022-01-14 23:48:12 +0300

    V7

--- src/main/java/com/techproed/java_dev_summer_tr/SecurityConfigurer.java
+++ src/main/java/com/techproed/java_dev_summer_tr/SecurityConfigurer.java
@@ -1,10 +1,16 @@
 package com.techproed.java_dev_summer_tr;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
@@ -15,5 +21,24 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
         auth.inMemoryAuthentication().withUser("admin").password(encoder.encode("12345")).roles("ADMIN");
 
+    }
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().addFilterAfter(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests().antMatchers("/authenticate").permitAll()
+                .anyRequest().authenticated();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
