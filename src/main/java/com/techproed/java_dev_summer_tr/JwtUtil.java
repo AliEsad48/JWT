b6f7cc1c5e89cd369d718ed853d825c4e2e36617
Salih Bayram <alicamci2020@gmail.com>
2022-01-14 22:20:48 +0300

    V4

--- src/main/java/com/techproed/java_dev_summer_tr/JwtUtil.java
+++ src/main/java/com/techproed/java_dev_summer_tr/JwtUtil.java
@@ -1,7 +1,37 @@
 package com.techproed.java_dev_summer_tr;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.Cipher;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
 public class JwtUtil {
 
+    private String SECRET_KEY = "summertrsummertrsummertrsummertrsummertrsummertrsummertrsummertrsummertr";
 
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
 
 }
