c11cd6c50bd0d228c7ae0843f214b6032ee15ce7
Salih Bayram <alicamci2020@gmail.com>
2022-01-14 23:18:15 +0300

    V6

--- src/main/java/com/techproed/java_dev_summer_tr/AuthenticationResponse.java
+++ src/main/java/com/techproed/java_dev_summer_tr/AuthenticationResponse.java
@@ -2,6 +2,15 @@ package com.techproed.java_dev_summer_tr;
 
 public class AuthenticationResponse {
 
-    private String jwt;
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
 
 }
--- src/main/java/com/techproed/java_dev_summer_tr/StudentController.java
+++ src/main/java/com/techproed/java_dev_summer_tr/StudentController.java
@@ -1,7 +1,17 @@
 package com.techproed.java_dev_summer_tr;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.RowSet;
 
 @RestController
 public class StudentController {
@@ -11,4 +21,20 @@ public class StudentController {
         return "Ali Can, Veli Han,Kerem Tan";
     }
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @PostMapping(value = "/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+        final UserDetails userDetails = myUserDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
 }
